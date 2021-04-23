bd7381655df00f989e28e721b5bc1cf722a311d1
fernandoum <fernanditososa@gmail.com>
2021-04-23 10:57:52 -0300

    install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo apt install nodejs
+sudo apt install npm
+sudo npm install -g coinmon
+
+#$ git clone https://github.com/bichenkk/coinmon.git
+#cd coinmon
+ yarn
+ npm install -g
+ npm link
+ coinmon
