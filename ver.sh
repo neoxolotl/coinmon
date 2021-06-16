a03e84641ebadd62e16edd2731e44123fd6442c6
fernandoum <fernanditososa@gmail.com>
2021-06-16 15:37:57 -0300

    filas

--- ver.sh
+++ ver.sh
@@ -5,6 +5,9 @@ do
 	echo "Press [CTRL+C] to stop.."
 	echo "-------------------------"
 	cat /proc/loadavg
-	coinmon
+	#coinmon 
+	#s/p = 10 
+	# -f bnb --> busca solo bnb 
+	coinmon -7
 	sleep 1m 
 done
